b9bcf24ecf61f0af6237d6e8a7d3148ddbb026aa
Berat Aktas <berataktas2834@gmail.com>
2025-05-11 00:17:10 +0300

    11.05.2025-DbContext sınıfı eklenmesi ve Entity Package dahil edilmesi

---
+++ E-Command/Models/Class/Context.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace E_Command.Models.Class
+
+{
+	public class Context:DbContext
+	{
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<Admin> Admins { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<HomePage> HomePages { get; set; }
+		public DbSet<Product> Products { get; set; }
+		public DbSet<SocialMedia> SocialMedias {  get; set; } 
+	}
+}
