2c2e0b58ab58e9fd775a9b7902b9be3745b07cd6
Berat Aktas <berataktas2834@gmail.com>
2025-05-25 18:06:50 +0300

    25.05.2025

---
+++ E-Command/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using E_Command.Models.Class;
+
+namespace E_Command.Controllers
+{
+	public class AboutController : Controller
+	{
+		Context c = new Context();
+		public IActionResult Index()
+		{
+			var value = c.Abouts.ToList();
+			return View(value);
+		}
+	}
+}
